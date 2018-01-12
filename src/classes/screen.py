e70ec1791dce71ea0ba7a911b6bc300b9426c9b2
Eduardo Fernandes <failcake.work@gmail.com>
2018-01-12 16:46:30 +0000

    Fixing issues
    Signed-off-by: Eduardo Fernandes <failcake.work@gmail.com>

--- src/classes/screen.py
+++ src/classes/screen.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import unicornhat as unicorn
-import webcolors as col
 
 """
 #################
@@ -27,15 +26,14 @@ class Screen(object):
     def isInsideScreen(self, x, y):
         return x > self.width or y > self.height or x < 0 or y < 0
 
-    def setPixel(self, x, y, hex):
+    def setPixel(self, x, y, r, g, b):
         if not self.isInsideScreen(x,y):
             return
 
-        r,g,b = col.hex_to_rgb(hex)
-        unicorn.set_pixel(x,y,r,g,b)
+        unicorn.set_pixel(x, y, int(r), int(g), int(b))
         unicorn.show()
 
     def getPixel(self, x, y):
-        if not self.isInsideScreen(x,y):
+        if not self.isInsideScreen(x, y):
             return
         return unicorn.get_pixel(x, y)
